062804bffd46ef8a3c07fc3782e628bf50193294
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2024-02-13 12:31:24 -0300

    :lipstick: style: estilizada página principal

--- src/App.tsx
+++ src/App.tsx
@@ -29,67 +29,90 @@ function App() {
 
   return (
     <main>
-      <h1>Planejador Monetário</h1>
+      <div className="card">
+        <h1 className="title">Planejador Monetário</h1>
 
-      <div>
-        <label htmlFor="totalAmt">Insira o total ganho:</label>
-        <input
-          type="number"
-          name="totalAmt"
-          id="totalAmt"
-          min={0}
-          value={totalAmt || 0}
-          onBlur={(event) =>
-            handleNegativeAmt(parseInt(event.target.value), setTotalAmt)
-          }
-          onChange={(event) => setTotalAmt(parseInt(event.target.value))}
-        />
-      </div>
+        <div className="div__inputs">
+          <label className="text" htmlFor="totalAmt">
+            Insira o total ganho:
+          </label>
+          <input
+            type="number"
+            className="input-text"
+            name="totalAmt"
+            id="totalAmt"
+            min={0}
+            value={totalAmt || 0}
+            onBlur={(event) =>
+              handleNegativeAmt(parseInt(event.target.value), setTotalAmt)
+            }
+            onChange={(event) => setTotalAmt(parseInt(event.target.value))}
+          />
+        </div>
 
-      <div>
-        <p>Escolha quanto você quer poupar:</p>
+        <div className="div__inputs">
+          <p className="text">Escolha quanto você quer poupar:</p>
 
-        <button
-          name="5"
-          onClick={(event) => setSavePer(parseInt(event.target.name))}
-        >
-          5%
-        </button>
+          <button
+            className="text button-style"
+            name="5"
+            onClick={(event) => setSavePer(parseInt(event.target.name))}
+          >
+            5%
+          </button>
 
-        <button
-          name="10"
-          onClick={(event) => setSavePer(parseInt(event.target.name))}
-        >
-          10%
-        </button>
-      </div>
+          <button
+            className="text button-style"
+            name="10"
+            onClick={(event) => setSavePer(parseInt(event.target.name))}
+          >
+            10%
+          </button>
+        </div>
 
-      <div>
-        <label htmlFor="curDay">Dia atual do mês:</label>
-        <input
-          type="number"
-          name="curDay"
-          id="curDay"
-          min={1}
-          max={31}
-          value={curDay || 1}
-          onBlur={(event) =>
-            handleNegativeAmt(parseInt(event.target.value), setCurDay)
-          }
-          onChange={(event) => setCurDay(parseInt(event.target.value))}
-        />
+        <div className="div__inputs">
+          <label className="text" htmlFor="curDay">
+            Dia atual do mês:
+          </label>
+          <input
+            type="number"
+            className="input-text"
+            name="curDay"
+            id="curDay"
+            min={1}
+            max={31}
+            value={curDay || 1}
+            onBlur={(event) =>
+              handleNegativeAmt(parseInt(event.target.value), setCurDay)
+            }
+            onChange={(event) => setCurDay(parseInt(event.target.value))}
+          />
 
-        <label htmlFor="lastDay">Último dia do mês:</label>
-        <select
-          name="lastDay"
-          id="lastDay"
-          onChange={(event) => setLastDay(parseInt(event.target.value))}
-        >
-          <option value={31}>31</option>
-          <option value={30}>30</option>
-          <option value={28}>28</option>
-          <option value={29}>29</option>
-        </select>
+          <label className="text" htmlFor="lastDay">
+            Último dia do mês:
+          </label>
+          <select
+            name="lastDay"
+            className="input-text"
+            id="lastDay"
+            onChange={(event) => setLastDay(parseInt(event.target.value))}
+          >
+            <option className="text" value={31}>
+              31
+            </option>
+            <option className="text" value={30}>
+              30
+            </option>
+            <option className="text" value={28}>
+              28
+            </option>
+            <option className="text" value={29}>
+              29
+            </option>
+          </select>
+        </div>
+
+        <h2 className="subtitle">Total que pode ser gasto por dia: X</h2>
       </div>
     </main>
   );
