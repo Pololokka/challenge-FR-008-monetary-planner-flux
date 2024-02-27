240ec33f12ee5d294cb21bcd923b636dcdb6532c
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2024-02-27 11:58:25 -0300

    :recycle: refactor: retiradas algumas dependências do context

--- src/FluxCore/Contexts/plannerContext.tsx
+++ src/FluxCore/Contexts/plannerContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, useContext, useEffect, useReducer } from 'react';
-import { PlannerAction, plannerActions } from '../Actions/Planner';
+import { createContext, useContext, useReducer } from 'react';
+import { PlannerAction } from '../Actions/Planner';
 import { PlannerState, plannerReducer } from '../Reducers/Planner';
 
 interface PlannerStore {
